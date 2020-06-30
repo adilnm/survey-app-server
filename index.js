62d3a381147c75415ec97e2ddd4d4e5cb398a0e4
Adil Naimi <adilnm23@gmail.com>
2020-06-30 05:25:32 -0500

    Install mongoose

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose=require('mongoose')
+const keys=require('./services/passport')
 require("./services/passport");
 
+mongoose.connect(keys.mongoURI)
 const app = express();
 
 require("./routes/authRoutes")(app);
