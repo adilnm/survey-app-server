2963545345f5f920412f4a819b378be9b20330e4
Adil Naimi <adilnm23@gmail.com>
2020-06-30 05:51:35 -0500

    Create user model

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose=require('mongoose')
 const keys=require('./config/keys')
 require("./services/passport");
+require("./models/user");
+
 
 mongoose.connect(keys.mongoURI)
 const app = express();
