beae14fc57a008d05e72864cc5d83e86e5f6f1d5
Adil Naimi <adilnm23@gmail.com>
2020-06-30 05:30:30 -0500

    Fix typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose=require('mongoose')
-const keys=require('./services/passport')
+const keys=require('./config/keys')
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI)
