37ba6bf7fedacd53da2c695f6144974deaf9a0c7
Adil Naimi <adilnm23@gmail.com>
2020-07-01 23:36:03 -0500

    Deserilaize user

--- services/passport.js
+++ services/passport.js
@@ -7,6 +7,13 @@ const User = mongoose.model("users");
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
