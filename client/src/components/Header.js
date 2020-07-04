b348b41ec054a1f55d8c1b30e6d870464feb71cb
Adil Naimi <adilnm23@gmail.com>
2020-07-04 16:49:04 -0500

    Make the logo clickable

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -13,18 +14,23 @@ class Header extends Component {
           </li>
         );
       default:
-        return(
+        return (
           <li>
             <a href="/api/logout">Logout</a>
           </li>
-        )
+        );
     }
   }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
           <ul id="nav-mobile" className="right">
             {this.renderContent()}
           </ul>
