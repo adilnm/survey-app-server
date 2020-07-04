fdf8abdc5698a03b203ccb9bdc3eff4d00edf040
Adil Naimi <adilnm23@gmail.com>
2020-07-03 23:54:17 -0500

    Refactor the reducer

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
+import {compose} from 'redux';
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import reduxThunk from "redux-thunk";
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, {}, composeEnhancer(applyMiddleware(reduxThunk)));
 
 ReactDOM.render(
   <Provider store={store}>
